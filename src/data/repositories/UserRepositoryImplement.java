f8ca0575630cd35c0d29906572a6a7d63ae8a7da
Olawalesegun <olawalesegun0@gmail.com>
2023-07-07 22:45:17 +0100

    --------------

--- src/data/repositories/UserRepositoryImplement.java
+++ src/data/repositories/UserRepositoryImplement.java
@@ -1,4 +1,43 @@
 package data.repositories;
 
-public class UserRepositoryImplement {
+public class UserRepositoryImplement implements UserRepository {
+    private List<User> users = new ArrayList();
+    @Override
+    public User save(User user){
+        for(User user1: users){
+            if(user1.getUserName.equals(user.getUserName)){
+                users.add(user);
+                return user;
+            }
+            else{
+                users.add(user);
+                return user;
+            }
+        }
+        return null;
+    }
+    @Override
+    public User findBy(String userName){
+        for(User user: users){
+            if(user.getuserName.equals(userName)){
+                return user;
+            }
+        }
+        return null;
+    }
+    @Override
+    public void deleteBy(String userName){
+        User theUserToBeRemoved = null;
+        for(User user: users){
+            if(user.getUserName.equals(userName)){
+                theUserToBeRemoved = user;
+                users.remove(user);
+            }
+        }
+    }
+    @Override
+    public void deleteBy(User user){
+        users.remove(user);
+    }
+
 }
